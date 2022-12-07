52ff0301ada9a21516db39bedba25b60eb7f5cc5
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-12-07 13:04:42 -0500

    Create question5.cs

---
+++ question5.cs
@@ -0,0 +1,14 @@
+//question 5 for homework loops
+using System;
+
+class Program
+{
+ public static void Main(string[] args)	
+ {
+	
+	for (int b = 3; b <= 99; b += 2) {
+		Console.Write(b + " ");
+	} 
+	
+ } 
+}	
