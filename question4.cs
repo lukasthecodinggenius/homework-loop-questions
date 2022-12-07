0c659d9fb28f2bc6f348f6da2e1fc6b1d056943f
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-12-07 13:42:21 -0500

    Create question4.cs

---
+++ question4.cs
@@ -0,0 +1,12 @@
+//question 4 for homework loops
+using System;
+
+class Program
+{
+ public static void Main(string[] args)	
+ {
+	for (int a = 1; a <= 1024; a =a*2) {
+		Console.Write(a + " ");
+	}
+ } 
+}	
